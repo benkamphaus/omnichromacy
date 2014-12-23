bc6836d4a9c35212255bf9feaf99dbdd22c78c4e
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-12-23 12:11:08 -0700

    better perf from this

--- src/rs_clj/io.clj
+++ src/rs_clj/io.clj
@@ -1,25 +1,35 @@
 (ns rs-clj.io
-  (:require [clojure.core.matrix :refer :all]
+  (:require [clojure.core.matrix :refer [reshape]]
             [clojure.java.io :refer [input-stream]]
-            [nio.core :as nio]
-            [bytebuffer.buff :as bb])
-  (:import [org.apache.commons.io IOUtils]))
+            [nio.core :as nio])
+  (:import [org.apache.commons.io IOUtils]
+           [java.nio ByteBuffer]))
 
 (defn raw-read
   "Read every byte in file to heap ByteBuffer"
-  [f]
-  (-> (input-stream f)
-      (IOUtils/toByteArray)
-      (nio/byte-buffer)))
+  [f & {:keys [endian] or {endian :little-endian}}]
+  (let [channel (nio/channel f)
+        size (.size channel)
+        buf (ByteBuffer/allocate size)]
+        (.read channel buf 0)
+        (nio/set-byte-order! buf endian)
+        buf))
 
-(defn slurp-img-cube
-  "Read an image cube from a raw binary file. At present, a horribly
-  inefficient way to read a file. TODO: map that subs dt for appropriate
-  take fn"
-  [f dims dt]
-  (let [pix-count (reduce * dims)
-        buf (raw-read f)]
-    (reshape (repeatedly pix-count #(bb/take-short buf)) dims)))
+(defn slurp-binary
+  "Slurp binary file of shorts into array. TODO: expand to
+  accept other data types."
+  [f & {:keys [endian] or {endian :little-endian}}]
+  (let [bbuf (.asShortBuffer (.flip (raw-read f :endian endian)))
+        coerce '.asShortBuffer
+        maker short-array
+        buf (coerce bbuf)
+        arr (maker (.limit buf))]
+    (.get buf arr)
+   arr))
+
+(defn slurp-image
+  [f dims & {:keys [endian] or {endian :little-endian}}]
+  (reshape (slurp-binary f :endian endian) dims))
 
 (defn raw-dump
   "Dump every byte in bstream into file"
@@ -29,11 +39,12 @@
 (comment
 
   ; WIP notes will go here
+  (require 'rs-clj.io)
+  (in-ns 'rs-clj.io)
 
   ; simple bsq image
   (def _ramp (reshape (range 100000) [100 100 10]))
 
-  (def nireland-img-cube 
-    (slurp-img-cube "data/nireland.dat" [472 682 128] "s"))
-
+  (def nireland
+    (slurp-image "data/nireland.dat" [472 682 128]))
 )
