8cb59b80c31285e809e31cd072a216a740b47763
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-12-23 15:43:49 -0700

    ok to limit to these for now

--- project.clj
+++ project.clj
@@ -3,11 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opts ^:replace ["-Xmx8g" "-Xms1g"]
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [incanter "1.5.5"]
                  [net.mikera/core.matrix "0.29.1"]
-                 [clojurewerkz/buffy "1.0.0"]
-                 [bytebuffer "0.2.0"]
-                 [gloss "0.2.4"]
-                 [commons-io "2.4"]
+                 [net.mikera/vectorz-clj "0.26.2"]
                  [nio "1.0.3"]])
