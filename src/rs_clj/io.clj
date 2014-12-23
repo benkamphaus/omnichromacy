1da1c6baf15a38b66697b275c512f0ba9ac41dbb
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-12-22 22:22:14 -0700

    wip that heap errors for obv reasons but why are hard binary arrays so hard in clj?

---
+++ src/rs_clj/io.clj
@@ -0,0 +1,39 @@
+(ns rs-clj.io
+  (:require [clojure.core.matrix :refer :all]
+            [clojure.java.io :refer [input-stream]]
+            [nio.core :as nio]
+            [bytebuffer.buff :as bb])
+  (:import [org.apache.commons.io IOUtils]))
+
+(defn raw-read
+  "Read every byte in file to heap ByteBuffer"
+  [f]
+  (-> (input-stream f)
+      (IOUtils/toByteArray)
+      (nio/byte-buffer)))
+
+(defn raw-dump
+  "Dump every byte in bstream into file"
+  [bstream f]
+  nil)
+
+(defn slurp-img-cube
+  "Read an image cube from a raw binary file. At present, a horribly
+  inefficient way to read a file."
+  [f dims dt-str]
+  (let [pix-count (reduce * dims)
+        buf (raw-read f)]
+    (-> (bb/unpack buf (apply str (repeat pix-count dt-str)))
+        (reshape dims))))
+
+(comment
+
+  ; WIP notes will go here
+
+  ; simple bsq image
+  (def _ramp (reshape (range 100000) [100 100 10]))
+
+  (def nireland-img-cube 
+    (slurp-img-cube "data/nireland.dat" [472 682 128] "s"))
+
+)
---
+++ src/rs_clj/read.clj
@@ -0,0 +1,14 @@
+(ns rs-clj.io
+  (:require [clojure.core.matrix :refer :all]))
+
+(defn splurp-img-cube
+  (let arr 
+
+(comment
+
+  ; WIP notes will go here
+
+  ; simple bsq image
+  (def _ramp (reshape (range 100000) [100 100 10]))
+
+)
