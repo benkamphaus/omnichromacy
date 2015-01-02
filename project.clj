171515f2bdaf705da63ee1e2530868eb64dd03c0
Ben Kamphaus <ben.kamphaus@gmail.com>
2015-01-01 21:37:01 -0700

    update project name in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rs-clj "0.1.0-SNAPSHOT"
+(defproject omnichromacy "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
